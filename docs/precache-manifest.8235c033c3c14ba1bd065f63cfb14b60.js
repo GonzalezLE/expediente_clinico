d8fcd29b215506d5170099e179618146c8d2c8f8
GonzalezLE <gonzalez-3216@hotmail.com>
2020-09-03 12:41:55 -0500

    integracion del template AdminLTe3

---
+++ docs/precache-manifest.8235c033c3c14ba1bd065f63cfb14b60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44945140c8cb7c5968d9ad720dff7f77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3cfeac0903c1354ebdb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aff30140409bfc1c9083",
+    "url": "/static/js/2.1c705e95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c705e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cfeac0903c1354ebdb",
+    "url": "/static/js/main.9cd17445.chunk.js"
+  },
+  {
+    "revision": "c8fd486886a8618210e2",
+    "url": "/static/js/runtime-main.c33cbf95.js"
+  },
+  {
+    "revision": "28f7e31d41f353b3aaff1236c7108479",
+    "url": "/static/media/Logo.28f7e31d.png"
+  }
+]);
